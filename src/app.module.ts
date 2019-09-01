67d2c552f0d7a51eaf87049db69c9b9b7a48961c
Lucas Mösch <lucas.moesch@rwth-aachen.de>
2019-09-02 00:37:52 +0200

    Added reactor routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { QlcService } from './common/qlc.service';
 import { LoggingService } from './common/logging.service';
 import { StateService } from './state/state.service';
 import { StateController } from './state/state.controller';
+import { ReactorController } from './reactor/reactor.controller';
 
 @Module({
   imports: [],
-  controllers: [StateController],
+  controllers: [StateController, ReactorController],
   providers: [QlcService, LoggingService, StateService],
 })
 export class AppModule {}
--- src/reactor/reactor.controller.ts
+++ src/reactor/reactor.controller.ts
@@ -1,11 +1,47 @@
 import { Controller, Get, Post, Delete } from '@nestjs/common';
 import { LoggingService } from '../common/logging.service';
+import { StateService } from 'src/state/state.service';
 
 @Controller('reactor')
 export class ReactorController {
-    constructor(private readonly _logger: LoggingService) {
+    constructor(private readonly _logger: LoggingService, private readonly _stateService: StateService) {
 
     }
 
+    @Get('alert_pressure')
+    public async alertPressure():Promise<any> {
+        if (this._stateService.state == 'normal' || this._stateService.state == 'powersave') {
+            // Trigger Warning
+        }
+    }
+
+    @Get('alert_heat')
+    public async alertHeat(): Promise<any> {
+        if (this._stateService.state == 'normal' || this._stateService.state == 'powersave') {
+            // Trigger Warning
+        }
+    }
+
+    @Get('power_low')
+    public async powerLow(): Promise<any> {
+        if (this._stateService.state == 'normal' || this._stateService.state == 'shutdown') {
+            this._stateService.state = 'powersave';
+            // Trigger Warning
+        }
+    }
+
+    @Get('power_high')
+    public async powerHigh(): Promise<any> {
+        if (this._stateService.state == 'powersave' || this._stateService.state == 'shutdown') {
+            this._stateService.state = 'normal';
+        }
+    }
+
+    @Get('shutdown')
+    public async shutdown(): Promise<any> {
+        if (this._stateService.state == 'normal' || this._stateService.state == 'powersave') {
+            this._stateService.state = 'shutdown';
+        }
+    }
     
 }
